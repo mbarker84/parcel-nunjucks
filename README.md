2b5214a2ca26abd25032f3ac49661fdccebfd673
Michelle Barker <mbarker_84@hotmail.co.uk>
2021-11-23 12:25:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 📦 Punjucks
+
+Sorry about the terrible name. I love [ParcelJS](https://parceljs.org) but got sad that it doesn’t support HTML templating languages other than Pug. This project uses the [nunjucks-to-html](https://github.com/AlexisPuga/nunjucks-to-html) package to convert Nunjucks files to HTML during development with Parcel. It also includes [Tailwind CSS](https://tailwindcss.com/) and an SVG sprite generator, becuase I often need those things. Happy days 😎
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const rem = (pixels) => {
+  return `${pixels / 16}rem`
+}
+
+module.exports = {
+  purge: ['./src/*.html', './src/js/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      maxWidth: {
+        'container-lg': `${rem(1600)}`,
+        container: `${rem(1400)}`,
+        '1/2-container': `${rem(1400 / 2)}`,
+        '1/3-container': `${rem(1400 / 3)}`,
+      },
+      width: {
+        'container-lg': `${rem(1600)}`,
+        container: `${rem(1400)}`,
+        '1/2-container': `${rem(1400 / 2)}`,
+        '1/3-container': `${rem(1400 / 3)}`,
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  corePlugins: {
+    container: false,
+  },
+}
